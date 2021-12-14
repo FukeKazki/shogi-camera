05b47a38a516cb69ed328d2d26667f9234345274
FukeKazki <kazkichi0906@gmail.com>
2021-12-14 13:20:37 +0900

    Fix: 型周りの修正 (#1)

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,21 +1,16 @@
-import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
+import { InferGetServerSidePropsType, NextPage } from "next";
 import { fetchGames } from "../../apis/firebase/games";
-import { GamesInfoType } from "../../types";
 
 export type GamesPageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
-type Props = {
-  games : GamesInfoType[]
-}
-
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const games = await fetchGames()
+export async function getServerSideProps() {
+  const games = await fetchGames();
   return {
     props: { games },
   };
-};
+}
 
-const Games: NextPage<GamesPageProps> = ({ games } : GamesPageProps) => {
+const Games: NextPage<GamesPageProps> = ({ games }) => {
   return (
     <>
       <div>games</div>
