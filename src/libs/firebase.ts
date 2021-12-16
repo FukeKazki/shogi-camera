a63630b3ddebe2edb275ec3c118d404fce238e02
FukeKazki <kazkichi0906@gmail.com>
2021-12-16 15:14:38 +0900

    Fix: フォーマット

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,5 +1,5 @@
-import {initializeApp} from "firebase/app"
-import {getFirestore} from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -8,8 +8,8 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MEESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const firebase = initializeApp(firebaseConfig);
-export const db = getFirestore()
+export const db = getFirestore();
---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type GamesInfoType = {
+  startTime: string;
+  endTime: string;
+  handicap: string;
+  id: string;
+  status: string;
+  sente: string;
+  gote: string;
+};
+
+export const instanceOfGamesInfo = (arg: any): arg is GamesInfoType =>
+  "startTime" in arg &&
+  "endTime" in arg &&
+  "handicap" in arg &&
+  "id" in arg &&
+  "status" in arg &&
+  "sente" in arg &&
+  "gote" in arg;
